d3e5f3142b0fefd34d22a199a9878d472c0a6fba
Matthew Johnson <matthew.johnson44@pcc.edu>
2017-02-07 21:05:47 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -3,5 +3,9 @@ package edu.pcc.cis.helloworld;
 /**
  * Created by Matthew Johnson on 2/7/2017.
  */
-public class HelloWorld {
+public class HelloWorld
+{
+    public static void main (String [] args){
+        System.out.println("Hello, World!");
+    }
 }
