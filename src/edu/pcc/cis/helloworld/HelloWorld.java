9e06c69530dd296b9ac4629be7fc3d76579cabfb
Matthew Johnson <matthew.johnson44@pcc.edu>
2017-02-07 21:10:01 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Matthew Johnson on 2/7/2017.
  */
@@ -7,5 +9,6 @@ public class HelloWorld
 {
     public static void main (String [] args){
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
